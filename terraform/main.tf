3609d3eb0575c01892302ef847def4d8832ce7e4
Yang <yiyanghsu@gmail.com>
2026-05-11 23:27:56 -0400

    week 7 and week 8

--- terraform/main.tf
+++ terraform/main.tf
@@ -112,3 +112,113 @@ resource "aws_iam_role_policy_attachment" "secrets_attach" {
 
   policy_arn = aws_iam_policy.secrets_manager_policy.arn
 }
+
+resource "aws_cloudfront_distribution" "api_distribution" {
+
+  enabled = true
+
+  web_acl_id = aws_wafv2_web_acl.api_waf.arn
+
+  origin {
+
+    domain_name = "${aws_apigatewayv2_api.lambda_api.id}.execute-api.us-east-1.amazonaws.com"
+
+    origin_id = "apiGatewayOrigin"
+
+    origin_path = "/prod"
+
+    custom_origin_config {
+
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+
+      origin_ssl_protocols = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+
+    allowed_methods = [
+      "GET",
+      "HEAD",
+      "OPTIONS"
+    ]
+
+    cached_methods = [
+      "GET",
+      "HEAD"
+    ]
+
+    target_origin_id = "apiGatewayOrigin"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+
+      query_string = true
+
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+resource "aws_wafv2_web_acl" "api_waf" {
+
+  name  = "secure-ai-api-waf"
+  scope = "CLOUDFRONT"
+
+  default_action {
+    allow {}
+  }
+
+  visibility_config {
+
+    cloudwatch_metrics_enabled = true
+
+    metric_name = "SecureAIApiWAF"
+
+    sampled_requests_enabled = true
+  }
+
+  rule {
+
+    name     = "RateLimitRule"
+    priority = 1
+
+    action {
+      block {}
+    }
+
+    statement {
+
+      rate_based_statement {
+
+        limit              = 100
+        aggregate_key_type = "IP"
+      }
+    }
+
+    visibility_config {
+
+      cloudwatch_metrics_enabled = true
+
+      metric_name = "RateLimitRule"
+
+      sampled_requests_enabled = true
+    }
+  }
+}
+
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,8 @@
 output "api_url" {
   value = aws_apigatewayv2_stage.prod.invoke_url
 }
+
+output "cloudfront_url" {
+
+  value = aws_cloudfront_distribution.api_distribution.domain_name
+}
